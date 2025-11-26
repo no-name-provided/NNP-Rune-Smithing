ed00cc0f848fe4087fcefededd9d78fe89091906
No Name Provided <>
2025-11-26 04:22:25 -0800

    Finished first pass at rune effects on armor. More planned later.

--- src/main/java/com/github/no_name_provided/nnp_rune_smithing/common/Events.java
+++ src/main/java/com/github/no_name_provided/nnp_rune_smithing/common/Events.java
@@ -2,29 +2,27 @@ package com.github.no_name_provided.nnp_rune_smithing.common;
 
 import com.github.no_name_provided.nnp_rune_smithing.common.capabilities.CastingTableCapability;
 import com.github.no_name_provided.nnp_rune_smithing.common.capabilities.MelterCapability;
-import com.github.no_name_provided.nnp_rune_smithing.common.data_components.RuneData;
 import com.github.no_name_provided.nnp_rune_smithing.common.data_components.RunesAdded;
 import com.github.no_name_provided.nnp_rune_smithing.common.entities.RSEntities;
 import com.github.no_name_provided.nnp_rune_smithing.common.items.runes.AbstractRuneItem;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.entity.ai.attributes.AttributeInstance;
-import net.minecraft.world.entity.ai.attributes.AttributeMap;
-import net.minecraft.world.entity.ai.attributes.AttributeModifier;
+import net.minecraft.util.Mth;
+import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.item.ItemStack;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.capabilities.Capabilities;
 import net.neoforged.neoforge.capabilities.RegisterCapabilitiesEvent;
+import net.neoforged.neoforge.event.AnvilUpdateEvent;
+import net.neoforged.neoforge.event.entity.living.ArmorHurtEvent;
 import net.neoforged.neoforge.event.entity.living.LivingEquipmentChangeEvent;
 
-import java.util.ArrayList;
+import java.util.Map;
 
-import static com.github.no_name_provided.nnp_rune_smithing.common.RSAttributeModifiers.WARD_RUNE_ABSORPTION;
-import static com.github.no_name_provided.nnp_rune_smithing.common.RSAttributeModifiers.wardRuneAbsorption;
+import static com.github.no_name_provided.nnp_rune_smithing.common.RSAttributeModifiers.*;
 import static com.github.no_name_provided.nnp_rune_smithing.common.data_components.RSDataComponents.RUNES_ADDED;
-import static com.github.no_name_provided.nnp_rune_smithing.common.items.RSItems.WARD_RUNE;
-import static com.github.no_name_provided.nnp_rune_smithing.common.items.RSItems.WIELD_RUNE;
+import static com.github.no_name_provided.nnp_rune_smithing.common.items.RSItems.*;
 
 @EventBusSubscriber
 public class Events {
@@ -45,11 +43,33 @@ public class Events {
                 // Docs say double, instance constructor takes double, value retrieved/set is float?
                 double absorptionChange = 0;
                 float absorptionPerTier = 1.0f;
+                double speedChange = 0;
+                float speedPerTier = 1.0f;
+                double underwaterMiningSpeedChange = 0;
+                float underwaterMiningSpeedPerTier = 1.0f;
+                double extraAirChange = 0;
+                float extraAirPerTier = 1.0f;
+                double waterSpeedChange = 0;
+                float extraWaterSpeedPerTier = 0.5f;
+                double healthChange = 0;
+                float healthPerTier = 1.0f;
+                double burnTimeMultChange = 0;
+                float burnTimeMultPerTier = -0.2f;
                 if (!oldRunes.equals(RunesAdded.DEFAULT.get())) {
                     if (oldRunes.target().rune() == WIELD_RUNE.get()) {
                         AbstractRuneItem rune = oldRunes.effect().rune();
                         if (rune == WARD_RUNE.get()) {
                             absorptionChange -= absorptionPerTier * oldRunes.effectiveTier();
+                        } else if (rune == AIR_RUNE.get()) {
+                            speedChange -= speedPerTier * oldRunes.effectiveTier();
+                        } else if (rune == WATER_RUNE.get()) {
+                            underwaterMiningSpeedChange -= underwaterMiningSpeedPerTier * oldRunes.effectiveTier();
+                            extraAirChange -= extraAirPerTier * oldRunes.effectiveTier();
+                            waterSpeedChange -= extraWaterSpeedPerTier * oldRunes.effectiveTier();
+                        } else if (rune == EARTH_RUNE.get()) {
+                            healthChange -= healthPerTier * oldRunes.effectiveTier();
+                        } else if (rune == FIRE_RUNE.get()) {
+                            burnTimeMultChange -= burnTimeMultPerTier * oldRunes.effectiveTier();
                         }
                     }
                 }
@@ -58,51 +78,58 @@ public class Events {
                         AbstractRuneItem rune = newRunes.effect().rune();
                         if (rune == WARD_RUNE.get()) {
                             absorptionChange += absorptionPerTier * newRunes.effectiveTier();
+                        } else if (rune == AIR_RUNE.get()) {
+                            speedChange += speedPerTier * newRunes.effectiveTier();
+                        } else if (rune == WATER_RUNE.get()) {
+                            underwaterMiningSpeedChange += underwaterMiningSpeedPerTier * newRunes.effectiveTier();
+                            extraAirChange += extraAirPerTier * newRunes.effectiveTier();
+                            waterSpeedChange += extraWaterSpeedPerTier * newRunes.effectiveTier();
+                        } else if (rune == EARTH_RUNE.get()) {
+                            healthChange += healthPerTier * newRunes.effectiveTier();
+                        } else if (rune == FIRE_RUNE.get()) {
+                            burnTimeMultChange += burnTimeMultPerTier * newRunes.effectiveTier();
                         }
                     }
                 }
-                AttributeMap attributes = player.getAttributes();
-                AttributeInstance absorption = attributes.getInstance(Attributes.MAX_ABSORPTION);
-                if (null != absorption) {
-                    // Not using convenience method here because IDE tells me it's unsafe, and Minecraft has a
-                    // remarkable way of nullifying things
-                    AttributeModifier oldModifier = absorption.getModifier(WARD_RUNE_ABSORPTION);
-                    if (null == oldModifier) {
-                        absorption.addTransientModifier(wardRuneAbsorption(absorptionChange));
-                        player.setAbsorptionAmount(player.getAbsorptionAmount() + (float)absorptionChange);
-                    } else if (!(oldModifier.amount() + (float) absorptionChange > 0)) {
-                        absorption.removeModifier(WARD_RUNE_ABSORPTION);
-                    } else {
-                        absorption.addOrUpdateTransientModifier(wardRuneAbsorption(oldModifier.amount() + (float)absorptionChange));
-                        player.setAbsorptionAmount(player.getAbsorptionAmount() + (float)absorptionChange);
-                    }
-                }
-                
-                // Works, but is inefficient and the absorption hearts setting can be cheesed
-//                ArrayList<RunesAdded> runes = new ArrayList<>(4);
-//                for (ItemStack armor : player.getArmorAndBodyArmorSlots()) {
-//                    RunesAdded rune = armor.get(RUNES_ADDED);
-//                    if (null != rune) {
-//                        runes.add(rune);
-//                    }
-//                }
-//                double absorptionBuff = 0;
-//                for (RunesAdded rune : runes) {
-//                    if (rune.target().rune() == WIELD_RUNE.get() && rune.effect().rune() == WARD_RUNE.get()) {
-//                        absorptionBuff += rune.effectiveTier() * 1.0f;
-//                    }
-//                }
-//                AttributeInstance absorptionAttribute = player.getAttributes().getInstance(Attributes.MAX_ABSORPTION);
-//                if (null != absorptionAttribute) {
-//                    if (absorptionBuff > 0) {
-//                        absorptionAttribute.addOrUpdateTransientModifier(wardRuneAbsorption(absorptionBuff));
-//                        // Placeholder, to avoid overwriting other modifiers effect. Really need to take a real
-//                        // crack at calculating change (and not just total)
-//                        player.setAbsorptionAmount(player.getMaxAbsorption());
-//                    } else {
-//                        absorptionAttribute.removeModifier(WARD_RUNE_ABSORPTION);
-//                    }
-//                }
+                updateAttribute(absorptionChange, player, RSAttributeModifiers::wardRuneAbsorption, WARD_RUNE_ABSORPTION, Attributes.MAX_ABSORPTION);
+                updateAttribute(speedChange, player, RSAttributeModifiers::airRuneSpeed, AIR_RUNE_SPEED, Attributes.MOVEMENT_SPEED);
+                updateAttribute(underwaterMiningSpeedChange, player, RSAttributeModifiers::waterRuneUWMiningSpeed, WATER_RUNE_UW_MINING_SPEED, Attributes.SUBMERGED_MINING_SPEED);
+                updateAttribute(extraAirChange, player, RSAttributeModifiers::waterRuneExtraAir, WATER_RUNE_EXTRA_AIR, Attributes.OXYGEN_BONUS);
+                updateAttribute(waterSpeedChange, player, RSAttributeModifiers::waterRuneExtraSwimSpeed, WATER_RUNE_EXTRA_SWIM_SPEED, Attributes.WATER_MOVEMENT_EFFICIENCY);
+                updateAttribute(healthChange, player, RSAttributeModifiers::earthRuneHealthChange, EARTH_RUNE_HEALTH, Attributes.MAX_HEALTH);
+                updateAttribute(burnTimeMultChange, player, RSAttributeModifiers::fireRuneBurnTimeMultChange, FIRE_RUNE_BURNING_TIME, Attributes.BURNING_TIME);
+            }
+        }
+    }
+    
+    @SubscribeEvent
+    static void onArmorHurt(ArmorHurtEvent event) {
+        // Flat reductions of even 1/tier are too powerful
+        float damageReductionPerTier = Mth.randomBetween(event.getEntity().getRandom(), 0f, 1.0f);
+        Map<EquipmentSlot, ArmorHurtEvent.ArmorEntry> map = event.getArmorMap();
+        map.keySet().forEach(slot -> {
+            RunesAdded runes = map.get(slot).armorItemStack.get(RUNES_ADDED);
+            if (null != slot && null != runes && runes.target().rune() == SELF_RUNE.get() && runes.effect().rune() == WARD_RUNE.get()) {
+                event.setNewDamage(slot, Mth.clamp(map.get(slot).originalDamage, 0, Math.max(0, map.get(slot).originalDamage - runes.effectiveTier() * damageReductionPerTier)));
+            }
+        });
+    }
+    
+    /**
+     * Item stacks aren't editable in the anvil repair event.
+     */
+    @SubscribeEvent
+    static void onAnvilUpdate(AnvilUpdateEvent event) {
+        int damageReductionPerTier = 10;
+        ItemStack input = event.getLeft();
+        ItemStack oldOutput = event.getOutput();
+        // Verify it's a successful repair recipe
+        if (input.getItem() == oldOutput.getItem() && oldOutput.getDamageValue() < input.getDamageValue()) {
+            RunesAdded runes = input.get(RUNES_ADDED);
+            if (null != runes && runes.target().rune() == SELF_RUNE.get() && runes.effect().rune() == EARTH_RUNE.get()) {
+                ItemStack newOutput = oldOutput.copy();
+                newOutput.setDamageValue(Mth.clamp(oldOutput.getDamageValue() - damageReductionPerTier * runes.effectiveTier(), 0, oldOutput.getDamageValue()));
+                event.setOutput(newOutput);
             }
         }
     }
--- src/main/java/com/github/no_name_provided/nnp_rune_smithing/common/RSAttributeModifiers.java
+++ src/main/java/com/github/no_name_provided/nnp_rune_smithing/common/RSAttributeModifiers.java
@@ -1,12 +1,22 @@
 package com.github.no_name_provided.nnp_rune_smithing.common;
 
+import net.minecraft.core.Holder;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.entity.ai.attributes.AttributeModifier;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.ai.attributes.*;
+
+import java.util.function.Function;
 
 import static com.github.no_name_provided.nnp_rune_smithing.NNPRuneSmithing.MODID;
 
 public class RSAttributeModifiers {
     public static ResourceLocation WARD_RUNE_ABSORPTION = ResourceLocation.fromNamespaceAndPath(MODID, "ward_rune_absorption");
+    public static ResourceLocation AIR_RUNE_SPEED = ResourceLocation.fromNamespaceAndPath(MODID, "air_rune_speed");
+    public static ResourceLocation WATER_RUNE_UW_MINING_SPEED = ResourceLocation.fromNamespaceAndPath(MODID, "water_rune_uw_mining_speed");
+    public static ResourceLocation WATER_RUNE_EXTRA_AIR = ResourceLocation.fromNamespaceAndPath(MODID, "water_rune_extra_air");
+    public static ResourceLocation WATER_RUNE_EXTRA_SWIM_SPEED = ResourceLocation.fromNamespaceAndPath(MODID, "water_rune_extra_swim_speed");
+    public static ResourceLocation EARTH_RUNE_HEALTH = ResourceLocation.fromNamespaceAndPath(MODID, "earth_rune_health");
+    public static ResourceLocation FIRE_RUNE_BURNING_TIME = ResourceLocation.fromNamespaceAndPath(MODID, "fire_rune_burning_time");
     
     public static AttributeModifier wardRuneAbsorption(double intensity) {
         return new AttributeModifier(
@@ -15,6 +25,74 @@ public class RSAttributeModifiers {
                 AttributeModifier.Operation.ADD_VALUE
         );
     }
+    public static AttributeModifier airRuneSpeed(double intensity) {
+        return new AttributeModifier(
+                AIR_RUNE_SPEED,
+                intensity,
+                AttributeModifier.Operation.ADD_VALUE
+        );
+    }
+    public static AttributeModifier waterRuneUWMiningSpeed(double intensity) {
+        return new AttributeModifier(
+                WATER_RUNE_UW_MINING_SPEED,
+                intensity,
+                AttributeModifier.Operation.ADD_VALUE
+        );
+    }
+    public static AttributeModifier waterRuneExtraAir(double intensity) {
+        return new AttributeModifier(
+                WATER_RUNE_EXTRA_AIR,
+                intensity,
+                AttributeModifier.Operation.ADD_VALUE
+        );
+    }
     
-
+    public static AttributeModifier waterRuneExtraSwimSpeed(double intensity) {
+        return new AttributeModifier(
+                WATER_RUNE_EXTRA_SWIM_SPEED,
+                intensity,
+                AttributeModifier.Operation.ADD_VALUE
+        );
+    }
+    public static AttributeModifier earthRuneHealthChange(Double intensity) {
+        return new AttributeModifier(
+                EARTH_RUNE_HEALTH,
+                intensity,
+                AttributeModifier.Operation.ADD_VALUE
+        );
+    }
+    public static AttributeModifier fireRuneBurnTimeMultChange(Double intensity) {
+        return new AttributeModifier(
+                FIRE_RUNE_BURNING_TIME,
+                intensity,
+                AttributeModifier.Operation.ADD_VALUE
+        );
+    }
+    
+    public static void updateAttribute(double change, ServerPlayer player, Function<Double, AttributeModifier> getter, ResourceLocation modifierID, Holder<Attribute> attribute) {
+        AttributeMap attributes = player.getAttributes();
+        AttributeInstance attributesInstance = attributes.getInstance(attribute);
+        
+        if (null != attributesInstance) {
+            // Not using convenience method here because IDE tells me it's unsafe, and Minecraft has a
+            // remarkable way of nullifying things
+            AttributeModifier oldModifier = attributesInstance.getModifier(modifierID);
+            if (null == oldModifier) {
+                attributesInstance.addTransientModifier(getter.apply(change));
+                // Special case attributes that need to be increased to match new max
+                if (attribute == Attributes.MAX_ABSORPTION) {
+                    player.setAbsorptionAmount(player.getAbsorptionAmount() + (float) change);
+                }
+                // Just remove the modifier if it would somehow be negative
+            } else if (!(oldModifier.amount() + (float) change > 0)) {
+                attributesInstance.removeModifier(modifierID);
+            } else {
+                attributesInstance.addOrUpdateTransientModifier(getter.apply(oldModifier.amount() + (float) change));
+                // Special case attributes that need to be increased to match new max
+                if (attribute == Attributes.MAX_ABSORPTION) {
+                    player.setAbsorptionAmount(player.getAbsorptionAmount() + (float) change);
+                }
+            }
+        }
+    }
 }
