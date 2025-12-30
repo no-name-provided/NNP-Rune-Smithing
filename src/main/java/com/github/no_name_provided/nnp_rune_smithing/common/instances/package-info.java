6a17d06b8d90dbaea1a4d2bdd1783720e8c086d2
No Name Provided <>
2025-12-30 09:28:53 -0800

    Hopefully deleted the last of the IntelliJ copy-file glitch extraneous directories.

--- src/main/java/com/github/no_name_provided/nnp_rune_smithing/common/instances/package-info.java
+++
@@ -1,6 +0,0 @@
-@ParametersAreNonnullByDefault @MethodsReturnNonnullByDefault
-package com.github.no_name_provided.nnp_rune_smithing.common.instances;
-
-import net.minecraft.MethodsReturnNonnullByDefault;
-
-import javax.annotation.ParametersAreNonnullByDefault;
