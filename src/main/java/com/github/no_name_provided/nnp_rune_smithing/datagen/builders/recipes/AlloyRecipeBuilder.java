b0701678aa9a79ca1562a98b778a3854c41e103d
No Name Provided <>
2026-01-29 19:52:11 -0800

    Added support for data load conditions in AlloyRecipeBuilder.
    Broke rune display on swords while trying to reverse engineer vanilla transforms.

--- src/main/java/com/github/no_name_provided/nnp_rune_smithing/datagen/builders/recipes/AlloyRecipeBuilder.java
+++ src/main/java/com/github/no_name_provided/nnp_rune_smithing/datagen/builders/recipes/AlloyRecipeBuilder.java
@@ -10,6 +10,7 @@ import net.minecraft.data.recipes.RecipeBuilder;
 import net.minecraft.data.recipes.RecipeOutput;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
+import net.neoforged.neoforge.common.conditions.ICondition;
 import net.neoforged.neoforge.fluids.FluidStack;
 import net.neoforged.neoforge.fluids.crafting.SizedFluidIngredient;
 import org.jetbrains.annotations.Nullable;
@@ -21,12 +22,21 @@ public class AlloyRecipeBuilder implements RecipeBuilder {
     protected final FluidStack result;
     protected final SizedFluidIngredient input1;
     protected final SizedFluidIngredient input2;
+    protected final ICondition[] loadConditions;
     protected final Map<String, Criterion<?>> criteria = new LinkedHashMap<>();
     
-    public AlloyRecipeBuilder(FluidStack result, SizedFluidIngredient input1, SizedFluidIngredient input2) {
+    public AlloyRecipeBuilder(FluidStack result, SizedFluidIngredient input1, SizedFluidIngredient input2, ICondition[] loadConditions) {
         this.result = result;
         this.input1 = input1;
         this.input2 = input2;
+        this.loadConditions = loadConditions;
+    }
+    
+    /**
+     * Constructor for a NeoForge load condition free instance of this recipe (builder).
+     */
+    public AlloyRecipeBuilder(FluidStack result, SizedFluidIngredient input1, SizedFluidIngredient input2) {
+        this(result, input1, input2, new ICondition[0]);
     }
     
     @Override
@@ -54,6 +64,9 @@ public class AlloyRecipeBuilder implements RecipeBuilder {
         this.criteria.forEach(advancement::addCriterion);
         AlloyRecipe recipe = new AlloyRecipe(input1, input2, result);
         
+        if (loadConditions.length != 0) {
+            recipeOutput = recipeOutput.withConditions(loadConditions);
+        }
         recipeOutput.accept(id, recipe, advancement.build(id.withPrefix("recipe/")));
     }
 }
